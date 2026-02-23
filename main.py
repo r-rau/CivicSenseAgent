74eeca2d3aabc38a161ce15bf97895cd27832aa7
r-rau <ronsliferokz@gmail.com>
2026-02-23 06:26:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def send_to_telegram(text, image_url=None):
 def run_agent():
     newsapi = NewsApiClient(api_key=NEWS_API_KEY)
     client = genai.Client(api_key=GEMINI_API_KEY)
-    model = genai.GenerativeModel('gemini-2.0-flash')
 
     # 1. Search for Civic News
     query = '(civic sense OR "public etiquette") AND (failure OR learning)'
